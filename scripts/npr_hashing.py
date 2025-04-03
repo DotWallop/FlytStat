9c2838632dafa61cfd89b3bc96118c4ba5328a0c
Mats Edvin Aarø <79384776+DotWallop@users.noreply.github.com>
2025-04-03 23:15:57 +0200

    Function refactor to Excel format
    Refactored function to parse a .xlsx document instead, due to compability issues with CSV.
    Improved and clarified functionality and documentation.
    
    * Refactoring to Xlsx due to compability issues. Untested.
    * npr_hashing.py now functional, nitpicks, error handling left.
    * fixed truncated comment, refactored function for clarity
    * removed index col from pandas output

--- scripts/npr_hashing.py
+++ scripts/npr_hashing.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import pickle
 from pathlib import Path
-import csv
 
 '''
 Note: I had some help figuring this logic out from a developer friend of mine.
@@ -10,39 +9,42 @@ I also came to the conclusion that the most secure thing was to run this script 
 '''
 
 
-def hash_new_aliases():
-    # Read the triage CSV file
-    df = pd.read_csv("../data/sensitive/vestfoldtriage.csv", delimiter=";", on_bad_lines='skip', quoting=csv.QUOTE_NONE, low_memory=False, skipinitialspace=True)
+def hash_aliases_from_excel():
+    # Read the triage Excel file
+    df = pd.read_excel("../data/sensitive/vestfoldtriage.xlsx", usecols="A:AQ")
     # Uses 'pathlib' method Path to conveniently store boolean response if there already is a mapping table
     vestfoldtriage_hashed_path = Path("../data/vestfoldtriage_data_hashed.pkl")
 
-    # Checks for hashed file in data dir, and creates an empty dictionary if it doesn't.
+    # Uses Pathlib to check for hashed file in data dir, and creates an empty dictionary if it doesn't.
     if vestfoldtriage_hashed_path.exists():
+        # Opening file in read mode as a binary stream in order to unpickle and read the file (for adding new entries). Built with help from the pickle docs.
         with open(vestfoldtriage_hashed_path, "rb") as triage_data:
             vestfoldtriage_hash = pickle.load(triage_data)  # TODO: explain .load method
     else:
         vestfoldtriage_hash = {}
     # TODO: Burde jeg ha en sjekk for å se om alias_ids er tom (try/except)?
     # Initiating a variable 'alias_ids', which is a set of the 'NPR ID' column in the data source, then subtracts already hashed ID's - making a unique list of unhashed ID's.
-    alias_ids = set(df['NPR ID']) - vestfoldtriage_hash.keys()
-    start_index = len(vestfoldtriage_hash) + 1  # Counter variable in preparation for enumeration below
+    new_alias_ids = set(df['NPR ID']) - vestfoldtriage_hash.keys()
+    start_index = len(vestfoldtriage_hash) + 1  # Counter variable in preparation for enumeration below, returning first unhashed index.
 
     # Looping through the 'vestfoldtriage_hash' dictionary, replacing the NPR ID with unique alias, using pad formatting (':05')
-    for alias, npr_id in enumerate(alias_ids, start=start_index):
+    for alias, npr_id in enumerate(new_alias_ids, start=start_index):
         vestfoldtriage_hash[npr_id] = f"pasient_{alias:05}"
 
-    # Dumping the aliased pickle file (opening file again in write mode)
+    # Dumping the now aliased pickle file, opening again in write mode as binary stream. Dump = pickle
     with open(vestfoldtriage_hashed_path, "wb") as triage_data:
         pickle.dump(vestfoldtriage_hash, triage_data)
 
-    # Initializing new column, applying update to dataframe
-    df['Alias ID'] = df['NPR ID'].map(vestfoldtriage_hash)
+    # checking for dupe, Initializing new column, applying update to dataframe
+    if 'Alias ID' not in df.columns:
+        df.insert(0, 'Alias ID', df['NPR ID'].map(vestfoldtriage_hash))
+        # Dropping (deleting) original ID column in dataframe to protect identity
+    df = df.drop('NPR ID', axis=1)
 
-    # Dropping (deleting) original ID column in dataframe
-    df = df.drop("NPR ID", axis=1)
+    # Save as Excel
+    df.to_excel("../data/vestfoldtriage_data_hashed.xlsx", index=False)
+    print(f"Success! Saved {len(new_alias_ids)} ID's.")
 
-    # Save as CSV
-    df.to_csv("../data/vestfoldtriage_data_hashed.csv", index=False, sep=";")
-    print(f"Success! Saved {len(alias_ids)} ID's.")
-
-hash_new_aliases()
+# Main guard - A trick my developer friend taught me.. :)
+if __name__ == "__main__":
+    hash_aliases_from_excel()
