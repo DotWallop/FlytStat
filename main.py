ce2dfbf32604aea47fb68cb1b51efe741a28f830
DotWallop <matsedvin@me.com>
2025-04-04 14:01:35 +0200

    Added skeleton to main user route and suggestions for plots in plots.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 import pandas as pd
+from plots import *
+
+
+print("Velkommen!\nVelg ønsket funksjon:")
+# List of function options. 
+input_options = ("Søk opp statistikk", "Vis datagrunnlag")
+
+# Indexes 'input_options' list using the enumerate function, starting at 1 to make the positions 1-based. "\033[1m" in print statement outputs bold text.
+for index, value in enumerate(input_options, start=1):
+    print(f"\033[1m{index}:\033[1m  {value}")
+
+print()
+print()
+user_choice = int(input("Valg: "))
+
+# While I could do this with an if/else statement, a match-case is more readable. Mapped into variables 
+match user_choice:
+    case 1:
+        pass
+        # Add another dialogue prompt for which plot the user wants
+    
+    case 2:
+        pass
+        # Add a function (in own function file?) that returns the metadata
+        # Add meta info as an input option, min-max of source date column to see date range, number of patients, etc.
+
+    case _:
+        raise keyError(f"{str(user_choice)} is not a valid entry.")
+# Wrap entire pipeline in a while True-loop? Add a "return to main" within the subsections, that clears the terminal if possible?
---
+++ plots.py
@@ -0,0 +1,14 @@
+﻿import pandas as pd
+import matplotlib.pyplot as plt
+
+def patients_per_month():
+    pass
+
+# Pie charts - quantity of the most prevalent diagnoses that day? Input date?
+# - Maybe add an attribute if you want to see day, month or year - and inform the user through input() statements pre-call
+
+# Bar chart - Triage per day
+
+# Line plot or similar - patients per hour per triage category (input: date)
+
+# Test out matplotlib's subplot feature to make multiple plots!
