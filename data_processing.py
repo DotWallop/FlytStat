ea3398482fcf48b66f6148f36b336ce21f9ec1cf
DotWallop (Mats) <79384776+DotWallop@users.noreply.github.com>
2025-04-08 00:26:27 +0200

    Added spreadsheet import, started on data processing

--- data_processing.py
+++ data_processing.py
@@ -5,12 +5,23 @@ from pathlib import Path
 
 sheet_path = Path('data/vestfoldtriage_data_hashed.xlsx')
 
+# Check if spreadsheet exists, if so, load it into a Pandas dataframe
 if not sheet_path.exists():
-    raise ValueError
+    raise FileNotFoundError(f"Kan ikke finne angitt fil: {sheet_path.resolve()}!\nHar du kjørt 'npr_hashing.py'?")
+else:
+    df = pd.read_excel(sheet_path.name)
 
+# Convert "urgency/triage" columns into a number format for comparison
+urgency_level = {
+    "NotUrgent": 1,
+    "LessUrgent": 2,
+    "Urgent": 3,
+    "Resuscitation": 4
+}
+
+# TODO: Make new column in df with above data?
 
 
 class PatientData:
     def __init__(self, patient_alias):
         self.patient_alias = patient_alias
-
