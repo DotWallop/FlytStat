ce9df2852b5aca6aeb7e7fa81efc19c3e67037a9
DotWallop (Mats) <79384776+DotWallop@users.noreply.github.com>
2025-04-08 10:18:24 +0200

    Corrected excel import statement, added triage mapping

--- data_processing.py
+++ data_processing.py
@@ -3,23 +3,25 @@ import numpy as np
 from pathlib import Path
 # Possible classes OOP style: patient-oriented, hospital/system-oriented
 
+
 sheet_path = Path('data/vestfoldtriage_data_hashed.xlsx')
 
 # Check if spreadsheet exists, if so, load it into a Pandas dataframe
 if not sheet_path.exists():
     raise FileNotFoundError(f"Kan ikke finne angitt fil: {sheet_path.resolve()}!\nHar du kjørt 'npr_hashing.py'?")
 else:
-    df = pd.read_excel(sheet_path.name)
+    df = pd.read_excel(sheet_path)
 
-# Convert "urgency/triage" columns into a number format for comparison
 urgency_level = {
     "NotUrgent": 1,
     "LessUrgent": 2,
     "Urgent": 3,
     "Resuscitation": 4
 }
+# Mapping string keys to their respective int value to enable comparison
+df['first_triage_number'] = df["Resultat av første pretriage"].map(urgency_level)
 
-# TODO: Make new column in df with above data?
+
 
 
 class PatientData:
