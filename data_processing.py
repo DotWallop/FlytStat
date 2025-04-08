fbd489eb1c1fda225e5d89aa0fef73dc4aea497b
DotWallop (Mats) <79384776+DotWallop@users.noreply.github.com>
2025-04-08 14:46:12 +0200

    Added console verbosity, tidied up code, added exception handling for data writing

--- data_processing.py
+++ data_processing.py
@@ -19,14 +19,17 @@ def insert_triage_col(old_col):
     elif not old_col in df.columns:
         raise ValueError(f"'{old_col}' not found in DataFrame.")
     elif new_col_name in df.columns:
-        print(f"Existing column: '{new_col_name}' found. Exiting function.")
+        print(f"Existing column: '{new_col_name}' found. Skipping column.")
         return False
+
+    print(f"Mapping '{old_col}'.\n This might take some time ...")  # Console print
 
     # Mapping old_col string values to urgency_level int counterpart, inserts col right of old original col
     new_col_index = df.columns.get_loc(old_col) + 1  # TODO: Test get_loc
     mapped_urgency_values = df[old_col].map(urgency_level)
     df.insert(new_col_index, new_col_name, mapped_urgency_values)
 
+    print(f"Mapping complete: {mapped_urgency_values.count()} values mapped!") # Console print
 
 # Check if spreadsheet exists, if so, load it into a Pandas dataframe
 if not sheet_path.exists():
@@ -34,16 +37,26 @@ if not sheet_path.exists():
 else:
     df = pd.read_excel(sheet_path)
 
-urgency_level = {
-    "NotUrgent": 1,
-    "LessUrgent": 2,
-    "Urgent": 3,
-    "Resuscitation": 4
-}
-# Mapping string keys to their respective int value to enable comparison
-# df['first_triage_number'] = df['Resultat av første pretriage'].map(urgency_level)
-insert_triage_col('Resultat av første pretriage')
+urgency_level = { "NotUrgent": 1,
+                  "LessUrgent": 2,
+                  "Urgent": 3,
+                  "Resuscitation": 4
+                  }
 
+# Mapping string keys to their respective int value to enable comparison
+insert_triage_col('Resultat av første pretriage')
+insert_triage_col('Resultat av første legerespons')
+insert_triage_col('Resultat av første triage')
+insert_triage_col('Klinisk bekymring i første triage')
 
 # Outputs modified file to the excel file
-df.to_excel(sheet_path, index=False)
+try:
+    df.to_excel(sheet_path, index=False)
+except ValueError:
+    print(f"{sheet_path.name} not found. Please run 'scripts/npr_hashing.py' again to regenerate.")
+except PermissionError:
+    print(f"Could not write to file: permission denied. Do you have {sheet_path.name} open?")
+except Exception as e:
+    print(f"Unexpected error!\n{e}")
+else:
+    print("Successfully edited file.")
