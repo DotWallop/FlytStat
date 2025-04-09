beb51477bacdb20cd38f4981d92ab2a4595acc10
DotWallop (Mats) <79384776+DotWallop@users.noreply.github.com>
2025-04-09 14:01:02 +0200

    Completed and tested nested function month_day_input..

--- metrics.py
+++ metrics.py
@@ -27,7 +27,7 @@ def month_day_input_to_datetime() -> pd.Timestamp:
     """
     A top-level, nested function that prompts the user to input a month and date for data visualization.
     get_valid_month_day() -- Runs the input flow and validates it with simple RegEx, outputs month_input, day_input
-
+    get_year_from_month_and_day() -- Calculates the year based on month and day input
     """
     def get_valid_month_day():
         while True:  # Month and day wrapper
@@ -36,12 +36,12 @@ def month_day_input_to_datetime() -> pd.Timestamp:
                 _user_month_input = input("Måned: ")
 
                 # RegEx match statement. Self-made pattern, passed all my tests, so should be good.
-                regex_match = re.search(r"(0[1-9]|1[0-2])",_user_month_input)
+                regex_match_month = re.search(r"^(0[1-9]|1[0-2])$",_user_month_input)
 
-                if regex_match is None:
+                if regex_match_month is None:
                     print("Feil inntasting. Måned må være to siffer, og gyldig måned.\n")
                     continue
-                month_input = int(regex_match.group())  # Extracts only matched string, converts to INT => Removes leading zeros
+                month_input = int(regex_match_month.group())  # Extracts only matched string, converts to INT => Removes leading zeros
                 break  # Month
 
             while True:  # Day
@@ -49,13 +49,13 @@ def month_day_input_to_datetime() -> pd.Timestamp:
                 _user_day_input = input("Dato: ")
 
                 # No 30-31 validation as I did not find a reasonable way of doing this.
-                regex_match = re.search(r"(0[1-9]|[1-2][0-9]|3[0-1])",_user_day_input)
+                regex_match_day = re.search(r"^(0[1-9]|[1-2][0-9]|3[0-1])$",_user_day_input)
 
-                if regex_match is None:
+                if regex_match_day is None:
                     print("Feil inntasting. Dato må være to siffer, og en gyldig dato.\n")
                     continue
                 break  # Day
-            day_input = int(regex_match.group()) # Extracts only matched string, converts to INT => Removes leading zeros
+            day_input = int(regex_match_day.group()) # Extracts only matched string, converts to INT => Removes leading zeros
             break  # Wrapper
         return month_input, day_input
 
@@ -67,9 +67,14 @@ def month_day_input_to_datetime() -> pd.Timestamp:
         elif month_input < 3:
             calculated_year = 2025
         elif month_input == 3:
-            calculated_year = 2025 if day_input <= 10 else 2024
+            calculated_year = 2025 if day_input <= 9 else 2024
 
         return calculated_year
+    month, day = get_valid_month_day()
+    year = get_year_from_month_and_day(month, day)
+
+    timestamp = pd.Timestamp(f"{year}-{month}-{day}")
+    return timestamp
 
 
 
