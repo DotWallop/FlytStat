7652130eb8c0e7e1f7dec600e5372e5b2c16c26a
DotWallop (Mats) <79384776+DotWallop@users.noreply.github.com>
2025-04-11 17:03:16 +0200

    Continued work on user navigation logic

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 """
 Main page, handles CLI choice logic
 """
-main_input_options = ("Søk opp statistikk", "Vis datagrunnlag")  # List of function options  TODO: If time: patient search? List patient from ID etc.
-EXIT_OPTION = len(main_input_options) + 1  # Dynamically sets index for exit option into constant
-print(" == FlytStat ==\n\nVelg ønsket funksjon:")
+import pages.statistics_page as sub_stats
+main_page_choices = ("Vis statistikk", "Vis datagrunnlag")  # List of function options  TODO: If time: patient search? List patient from ID etc.
+exit_option = len(main_page_choices) + 1  # Dynamically sets index for exit option
+print("*"*18 + "\n     FlytStat     \n" + "*"*18)
 
+while True:
+    print("\nVelg ønsket funksjon:")
+    print("—"*23)  # Horizontal seperator
 
-# Indexes 'input_options' list using the enumerate function, starting at 1 to make the positions 1-based.
-for index, value in enumerate(main_input_options, start=1):
-    print(f"[{index}]  {value}")
-print(f"[{len(main_input_options)+1}]  Avslutt ...")  # Dynamically set
+    # Indexes 'input_options' list using the enumerate function, starting at 1 to make the positions 1-based.
+    for index, value in enumerate(main_page_choices, start=1):
+        print(f"[{index}]  {value}")
+    print(f"[{exit_option}]  Avslutt")  # Dynamically set
+    print("—"*23)  # Horizontal seperator
 
-print()
-print()
-user_choice = int(input("Valg: "))
+    try:
+        user_choice = int(input("Valg: "))
+    except ValueError:
+        print(f"Ugyldig valg... Vennligst skriv inn et tall mellom 1 og {len(main_page_choices)}.")
 
-# While I could do this with an if/else statement, a match-case is more readable.
-match user_choice:
-    case 1:
-        print(1)
-        # Add another dialogue prompt for which plot the user wants
-    
-    case 2:
-        print(2)
-        # Add a function (in own function file?) that returns the metadata
-        # Add meta info as an input option, min-max of source date column to see date range, number of patients, etc.
+    # While I could do this with an if/else statement, a match-case is more readable.
+    match user_choice:
+        case 1:
+            sub_stats.run()
+            input("Trykk på Enter-tasten for å returnere til menyen ...")  # A loop pause, preventing menu from automatically popping up
 
-    case EXIT_OPTION:
-        print("Bye!")
-    case _:
-        raise KeyError(f"{str(user_choice)} is not a valid entry.")
-# Wrap entire pipeline in a while True-loop? Add a "return to main" within the subsections, that clears the terminal if possible?
+        case 2:
+            print(2)
+            input("Trykk på Enter-tasten for å returnere til menyen ...")  # A loop pause, preventing menu from automatically popping up
+            # TODO: Add a function (in own function file?) that returns the metadata
+            # TODO: Add meta info as an input option, min-max of source date column to see date range, number of patients, etc.
+
+        case _ if user_choice == exit_option:  # After some trial and error I found this works, not sure if it is best practice.
+            break
+        case _:
+            print(f"Ugyldig valg... Vennligst skriv inn et tall mellom 1 og {len(main_page_choices)}.")
--- pages/statistics_page.py
+++ pages/statistics_page.py
@@ -1,18 +1,36 @@
 
 
 def run():
-    stats_page_choices = (
-        "Stablet stolpediagram -- Pasienter innlagt per time",  # TODO: Func name as comment
-        "Sektordiagram -- Andel av symptomtype per døgn"
-    )
+    while True:
+        stats_page_choices = ("Stablet stolpediagram -- Pasienter innlagt per time",  # TODO: Func name as comment
+                              "Sektordiagram -- Andel av symptomtype per døgn"
+                              )
+        exit_option = len(stats_page_choices) + 1
 
-    print("==  Tilgjengelig statistikk:  ==")
+        print("==  Tilgjengelig statistikk:  ==\n")
+        print("—" * 56)  # Horizontal seperator
+        for index, value in enumerate(stats_page_choices, start=1):
+            print(f"[{index}]  {value}")
+        print(f"[{exit_option}]  Tilbake til hovedmeny")  # Dynamic return option
+        print("—" * 56)  # Horizontal seperator
+        try:  # Gentle error handling for invalid inputs
+            user_choice = int(input("Valg: "))
+        except ValueError:
+            print(f"Ugyldig valg... Vennligst skriv inn et tall mellom 1 og {len(stats_page_choices)}.")
 
-    for index, value in enumerate(stats_page_choices, start=1):
-        print(f"[{index}]  {value}")
+        match user_choice:
+            case 1:
+                pass
+                input("Trykk på Enter-tasten for å returnere til menyen ...")  # A loop pause, preventing menu from automatically popping up
 
-    print(f"[{len(stats_page_choices)+1}]  tilbake til hovedmeny ...")  # Dynamic return option
+            case 2:
+                pass
+                input("Trykk på Enter-tasten for å returnere til menyen ...")  # A loop pause, preventing menu from automatically popping up
 
+            case _ if user_choice == exit_option:
+                break
+            case _:
+                raise KeyError(f"Ugyldig valg... Vennligst skriv inn et tall mellom 1 og {len(stats_page_choices)}.")
 
 if __name__ == "__main__":  # Main guard, enables to run functions if it's ran from file
     run()
